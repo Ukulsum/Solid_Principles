c2b532329e109cce84a702eae4c9372dbc90d139
ukulsum <ukulsum1280@gmail.com>
2025-01-18 02:35:12 +0600

    Add project files.

---
+++ Solid_Principles/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principles
+{
+    public class Employee
+    {
+        //public double CalculatePay(Employee emp)
+        //{
+
+        //}
+
+        //public void Save(Employee emp)
+        //{
+
+        //}
+
+        //public string ReportHours(Employee emp)
+        //{
+
+        //}
+    }
+}
---
+++ Solid_Principles/Invoice.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principles
+{
+    public class Invoice
+    {
+        public long InvAmount { get; set; }
+        public DateTime InvDate { get; set; }
+        private ILogger fileLogger;
+        private MailSender emailSender;
+        public Invoice()
+        {
+            fileLogger = new Logger();
+            emailSender = new MailSender();
+        }
+
+        public void AddInvoice()
+        {
+            try
+            {
+                fileLogger.Info("Add method Start");
+                // Here we need to write the Code for adding invoice
+                // Once the Invoice has been added, then send the  mail
+                emailSender.EMailFrom = "ukfarhana@gmail.com";
+                emailSender.EMailTo = "hello23@gmail.com";
+                emailSender.EMailSubject = "Single Responsibility Principle";
+                emailSender.EMailBody = "A class should have only one reason to change";
+                emailSender.SendEmail();
+            }
+            catch (Exception ex)
+            {
+                fileLogger.Error("Error Occurred while Generating Invoice", ex);
+            }
+        }
+
+        public void DeleteInvoice()
+        {
+            try
+            {
+                //Here we need to write the Code for Deleting the already generated invoice
+                fileLogger.Info("Delete Invoice Start at @" + DateTime.Now);
+            }
+            catch(Exception ex)
+            {
+                fileLogger.Error("Error Occurred while Deleting Invoice", ex);
+            }
+        }
+    }
+}
